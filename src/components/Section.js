06454a719ef6d4b14d025b92d919be03e10b62da
Maria Spiridonova <traffic2007@mail.ru>
2023-04-26 10:26:08 +0300

    8sprint 2

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,16 @@
 export default class Section {
-  constructor({ items, renderer }, containerSelector) {
+  constructor({ items, renderer }, container) {
     this._items = items;
     this._renderer = renderer;
 
-    this._containerSelector = document.querySelector(containerSelector);
+    this._container = document.querySelector(container);
   }
 
   renderItems() {
     this._items.forEach((item) => {this._renderer(item)});
   }
   addItem(element) {
-    this._containerSelector.prepend(element);
+    this._container.prepend(element);
   }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
 import "./index.css";
-import Card from "../scripts/Card.js";
-import { initialCards, options } from "../scripts/constants.js";
-import FormValidator from "../scripts/FormValidator.js";
-import PopupWithForm from "../scripts/PopupWithForm.js";
-import PopupWithImage from "../scripts/PopupWithImage.js";
-import Section from "../scripts/Section.js";
-import UserInfo from "../scripts/UserInfo.js";
+import Card from "../components/Card.js";
+import { initialCards, options } from "../components/constants.js";
+import FormValidator from "../components/FormValidator.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import Section from "../components/Section.js";
+import UserInfo from "../components/UserInfo.js";
 
 
 const buttonOpenEditProfilePopup = document.querySelector('.account__edit-button');
-/*const popupEdit = document.querySelector('.popup_edit');*/
 const buttonCloseEdit = document.querySelector('.popup__close');
 const formEditProfile = document.querySelector('.popup__form-edit');
-/*const popupAdd = document.querySelector('.popup_add');*/
 const buttonCloseAdd = document.querySelector('.popup__close-button-add');
 const buttonOpenAddCardPopup = document.querySelector('.account__add-button');
 const formAddCard = document.querySelector('.popup__form-add');
@@ -20,7 +18,6 @@ const userNameInput = document.querySelector('.popup__input_type_name');
 const userOccupationInput = document.querySelector('.popup__input_type_job');
 const nameCardsInput = document.querySelector('.popup__input_type_title');
 const linkCardsInput = document.querySelector('.popup__input_type_link');
-/*export const popupPhoto = document.querySelector('.popup_image');*/
 export const fullPhoto = document.querySelector('.popup__open-image');
 export const nameFullPhoto = document.querySelector('.popup__header-image');
 const buttonClosePhoto = document.querySelector('.popup__close-image');
@@ -28,94 +25,7 @@ const popupList = document.querySelectorAll('.popup');
 
 
 
-/*
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closePopupEscape);
 
-}
-
-export function openPopup(popup) {
-  popup.classList.add('popup_opened');
-  document.addEventListener('keydown', closePopupEscape);
-
-}
-
-const handleEditAccountClick = () => {
-
-  userNameInput.value = userNameElement.textContent;
-  userOccupationInput.value = userOccupationElement.textContent
-  openPopup(popupEdit);
-}
-const handleEditForm = (evt) => {
-  evt.preventDefault();
-  userNameElement.textContent = userNameInput.value;
-  userOccupationElement.textContent = userOccupationInput.value;
-  closePopup(popupEdit);
-}
-
-buttonOpenEditProfilePopup.addEventListener('click', handleEditAccountClick);
-buttonCloseEdit.addEventListener('click', () => {
-  closePopup(popupEdit);
-});
-formEditProfile.addEventListener('submit', handleEditForm);
-
-const posts = document.querySelector('.posts');
-
-
-function renderCard(data) {
-  const card = new Card(data, '#cardTemplate');
-  const cardElement = card.generateCard();
-
-  return cardElement;
-}
-
-initialCards.forEach((data) => {
-  posts.prepend(renderCard(data));
-});
-
-
-const handleAddForm = (evt) => {
-  evt.preventDefault();
-  const newCardObject = {};
-  newCardObject.name = nameCardsInput.value;
-  newCardObject.link = linkCardsInput.value;
-  posts.prepend(renderCard(newCardObject));
-  closePopup(popupAdd);
-}
-
-
-buttonOpenAddCardPopup.addEventListener('click', () => {
-  formAddCardValidator.inactiveButton();
-  formAddCard.reset();
-  openPopup(popupAdd);
-
-});
-
-
-buttonClosePhoto.addEventListener('click', () => {
-  closePopup(popupPhoto);
-});
-
-buttonCloseAdd.addEventListener('click', () => {
-  closePopup(popupAdd);
-});
-formAddCard.addEventListener('submit', handleAddForm);
-
-function closePopupEscape(evt) {
-  if (evt.key === 'Escape') {
-    closePopup(document.querySelector('.popup_opened'));
-  }
-
-}
-
-popupList.forEach((popup) => {
-  popup.addEventListener('mousedown', (evt) => {
-    if (evt.target === evt.currentTarget) {
-      closePopup(evt.target);
-    };
-  });
-});*/
 
 
 const formEditProfileValidator = new FormValidator(options, formEditProfile);
